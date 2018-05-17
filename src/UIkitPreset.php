7e2f08cf15e2ae7ff5c2a9b950201e870429277c
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-05-16 23:43:18 -0600

    :sparkles: Update to RC2

--- src/UIkitPreset.php
+++ src/UIkitPreset.php
@@ -44,7 +44,7 @@ class UIkitPreset extends Preset
     protected static function updatePackageArray(array $packages)
     {
         return [
-            'uikit' => '^3.0.0-rc.1',
+            'uikit' => '^3.0.0-rc.2',
         ] + Arr::except($packages, ['bootstrap-sass', 'bootstrap', 'jquery', 'popper.js']);
     }
 
